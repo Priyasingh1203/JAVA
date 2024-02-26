8af33f1831dd02abff7f8f4d133cbda1b14a7a46
PRIYA SINGH <115832273+Priyasingh1203@users.noreply.github.com>
2024-02-26 20:32:02 +0530

    Create LoginForm.java

---
+++ JFRAME/Login/LoginForm.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.loginform;
+
+/**
+ *
+ * @author SINGH'S FAMILY
+ */
+public class LoginForm {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
