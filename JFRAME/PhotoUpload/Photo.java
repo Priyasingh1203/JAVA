fba22812fd49c5d51f82806756e67b122fd61d19
PRIYA SINGH <115832273+Priyasingh1203@users.noreply.github.com>
2024-02-26 20:28:34 +0530

    Create Photo.java

---
+++ JFRAME/PhotoUpload/Photo.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package photo;
+
+/**
+ *
+ * @author SINGH'S FAMILY
+ */
+public class Photo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
